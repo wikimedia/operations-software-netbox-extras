d9005e8a72ed2491587b83787659bdd22512801b
Riccardo Coccioli <rcoccioli@wikimedia.org>
2020-06-24 20:02:51 +0200

    scripts: allow to offline multiple devices at once
    * Use also the same mechanism of interface_automation.py to return all
      the messages logged as output.
    
    Change-Id: Ic3b10e5bc9ab6f5f0742b95477ca67f41fe127fa

--- customscripts/offline_device.py
+++ customscripts/offline_device.py
@@ -1,4 +1,5 @@
 from dcim.models import Device
+from extras.constants import LOG_LEVEL_CODES
 from extras.scripts import Script, StringVar
 
 
@@ -9,26 +10,34 @@ class OfflineDevice(Script):
         description = ('Set the device status to Offline (unracked), unset its position and delete all interfaces '
                        'and related IP addresses.')
 
-    device_name = StringVar(label='Device name', description='Device to be offlined')
+    devices = StringVar(
+        label='Device name(s)',
+        description='Device to be offlined, space separated if more than one',
+    )
 
     def run(self, data):
         """Offline the device."""
         try:
-            message = self._run(data)
-            self.log_success(message)
+            self._run(data)
         except Exception as e:
-            message = 'Failed to offline device {name}: {e}'.format(name=data['device_name'], e=e)
-            self.log_failure(message)
+            self.log_failure('Failed to offline device(s) {name}: {e}'.format(name=data['device_name'], e=e))
 
-        return message
+        return self._format_logs()
 
     def _run(self, data):
         """Actually run the script."""
-        device = Device.objects.get(name=data['device_name'])
-        self.log_info('Found Netbox device')
+        devices = Device.objects.filter(name__in=data['devices'].split())
+        self.log_info('Found {n} Netbox devices'.format(n=len(devices)))
+
+        for device in devices:
+            self._run_device(device)
+
+    def _run_device(self, device):
+        """Run the script for one device."""
         if device.status != 'decommissioning':
-            raise RuntimeError('Device {name} is in {status} status, only decommissioned devices '
-                               'can be offlined.'.format(name=device, status=device.status))
+            self.log_failure(('Device {name} is in {status} status, only decommissioned devices '
+                              'can be offlined. Skipping device.').format(name=device, status=device.status))
+            return
 
         self.log_info('Setting device {device} status to Offline and unset rack/unit position'.format(device=device))
         device.status = 'offline'
@@ -38,13 +47,18 @@ class OfflineDevice(Script):
 
         for interface in device.interfaces.all():
             for address in interface.ip_addresses.all():
-                self.log_info('Deleting address {ip} with DNS name {dns} on interface {iface}'.format(
-                              ip=address, dns=address.dns_name, iface=interface))
+                self.log_info('Deleting address {ip} with DNS {dns} on interface {iface} for device {name}'.format(
+                              ip=address, dns=address.dns_name, iface=interface, name=device))
                 address.delete()
 
-            self.log_info('Deleting interface {iface}'.format(iface=interface))
+            self.log_info('Deleting interface {iface} on device {name}'.format(iface=interface, name=device))
             interface.delete()
 
         device.save()
-        return ('Successfully offlined device {name}, all interfaces and related IPs have been deleted').format(
-            name=device)
+        self.log_success('Successfully offlined device {name}'.format(name=device))
+
+    def _format_logs(self):
+        """Return all log messages properly formatted."""
+        return "\n".join(
+            "[{level}] {msg}".format(level=LOG_LEVEL_CODES.get(level), msg=message) for level, message in self.log
+        )
