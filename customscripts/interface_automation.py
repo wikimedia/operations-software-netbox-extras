9736485d08fb1a19e2ad67ceef82a08f27516aa1
Arzhel Younsi <ayounsi@wikimedia.org>
2020-11-19 17:02:48 +0100

    Add script to move a server within a row
    Tested on netbox-next
    
    Change-Id: Icd3b10e83d6b685e9daeb38f52c1f71b315a648d

--- customscripts/interface_automation.py
+++ customscripts/interface_automation.py
@@ -640,6 +640,28 @@ class Importer:
         self.log_success(f"{nbiface.device}: created cable {cable}")
         self.log_warning(f"{nbiface.device}: assuming {color_human} {cable_type} because {nbiface.type}")
 
+    def _update_z_iface(self, z_nbdevice, z_iface, vlan, type):
+        """Create switch interface if needed and set vlan info."""
+        try:
+            # Try to find if the interface exists
+            z_nbiface = z_nbdevice.interfaces.get(name=z_iface)
+        except ObjectDoesNotExist:  # If interface doesn't exist: create it
+            z_nbiface = Interface(name=z_iface,
+                                  mgmt_only=False,
+                                  device=z_nbdevice,
+                                  type=type)
+            z_nbiface.save()
+            self.log_success(f"{z_nbdevice}: created interface {z_iface}.")
+
+        # Then configure it
+        z_nbiface.mode = 'access'
+        z_nbiface.untagged_vlan = vlan
+        z_nbiface.enabled = True
+        z_nbiface.mtu = 9192
+        z_nbiface.save()
+        self.log_success(f"{z_nbiface.device}:{z_nbiface} configured vlan.")
+        return z_nbiface
+
 
 class ImportNetworkFacts(Script, Importer):
     class Meta:
@@ -868,6 +890,132 @@ class ProvisionServerNetworkCSV(Script):
         return row
 
 
+class MoveServer(Script, Importer):
+    class Meta:
+        name = "Move a server within the same row"
+        description = ("More exactly: keep the same vlan and IP.")
+
+    # TODO convert to NB 2.9 with proper select
+    device = ObjectVar(
+        required=True,
+        description=("Server. (Required)"),
+        queryset=Device.objects.filter(device_role__slug='server'),
+        widget=APISelect(
+            api_url="/api/dcim/devices/",
+            additional_query_params={
+                'role': 'server',
+            }
+        ),
+    )
+    # TODO convert to NB 2.9 with proper select, to only show the ToR of the selected device
+    z_nbdevice = ObjectVar(
+        required=True,
+        label="New switch",
+        description=("New top of rack switch. (Required)"),
+        queryset=Device.objects.filter(device_role__slug__in=('asw', 'cloudsw'), status__in=('active', 'staged')),
+        widget=APISelect(
+            api_url="/api/dcim/devices/",
+            additional_query_params={
+                'role': ('asw', 'cloudsw'),
+                'status': ('active', 'staged'),
+            }
+        ),
+    )
+    z_iface = StringVar(label="Switch interface", description="Switch interface. (Required)", required=True)
+
+    position = ChoiceVar(
+        required=True,
+        choices=[(i, i) for i in range(48)],
+        label="New rack unit",
+        description=("Rack will be the same as the new top of rack switch."),
+    )
+    cable_id = StringVar(label="Cable ID", required=False)
+
+    def run(self, data, commit):
+        """Run the script and return all the log messages."""
+        self.log_info(f"Called with parameters: {data}")
+        self.move_server(data)
+        return format_logs(self.log)
+
+    def move_server(self, data):
+        """Process a single device."""
+        device = data['device']
+        z_nbdevice = data['z_nbdevice']
+        z_iface = data['z_iface']
+        position = data['position']
+        cable_id = data['cable_id']
+
+        if not z_nbdevice or z_iface == '':
+            self.log_failure(
+                f"{device}: New switch or switch interface missing, skipping."
+            )
+            return
+
+        if z_nbdevice.status not in ('active', 'staged'):
+            self.log_failure(f"{device}: switch {z_nbdevice} with status {z_nbdevice.status}, "
+                             "expected Active or Staged, skipping.")
+            return
+
+        if int(position) == 0:
+            self.log_failure(f"{device}: Rack unit can't be 0, skipping.")
+            return
+
+        if device.device_role.slug != "server":
+            self.log_failure(
+                f"{device.name}: role {device.device_role}, only servers are supported, skipping."
+            )
+            return
+
+        if z_nbdevice.device_role.slug not in ('asw', 'cloudsw'):
+            self.log_failure(f"{device}: switch {z_nbdevice} with role {z_nbdevice.device_role}, "
+                             "only switches are supported, skipping.")
+            return
+
+        # find main interface
+        ifaces_connected = device.interfaces.filter(mgmt_only=False).exclude(cable=None)
+        if len(ifaces_connected) != 1:
+            ifaces_list = ", ".join(i.name for i in ifaces_connected)
+            self.log_failure(f"{device}: either 0 or more than 1 connected interface: {ifaces_list},"
+                             f" please update Netbox manually, skipping.")
+            return
+        # At this point there is only the one.
+        nbiface = ifaces_connected[0]
+        nbcable = nbiface.cable
+        # Find old switch interface, one one side or the other of the cable
+        if nbcable.termination_a != nbiface:
+            z_old_nbiface = nbcable.termination_a
+        elif nbcable.termination_b != nbiface:
+            z_old_nbiface = nbcable.termination_b
+
+        # Configure the new switch interface
+        z_nbiface = self._update_z_iface(z_nbdevice, z_iface, z_old_nbiface.untagged_vlan, z_old_nbiface.type)
+        if z_nbiface.cable:
+            self.log_failure(f"There is already a cable on {z_nbiface.device}:{z_nbiface} (typo?), skipping.")
+            return
+
+        # Clean the old one
+        z_old_nbiface.enabled = False
+        z_old_nbiface.mode = ''
+        z_old_nbiface.untagged_vlan = None
+        z_old_nbiface.mtu = None
+        z_old_nbiface.tagged_vlans.set([])
+        z_old_nbiface.save()
+        self.log_success(f"{device}: reset old switch interface {z_old_nbiface.device}:{z_old_nbiface}.")
+
+        # Remove the old cable
+        nbcable.delete()
+        self.log_success(f"{device}: deleted old cable.")
+
+        # Create the new one
+        self._create_cable(nbiface, z_nbiface, label=cable_id if cable_id else '')
+
+        # Don't forget to update the device rack/U
+        device.rack = z_nbdevice.rack
+        device.position = int(position)
+        device.save()
+        self.log_success(f"{device}: moved to rack {device.rack}, U{device.position}.")
+
+
 class ProvisionServerNetwork(Script, Importer):
 
     class Meta:
@@ -1048,28 +1196,6 @@ class ProvisionServerNetwork(Script, Importer):
 
         if assign_mgmt:
             self._assign_mgmt(device)
-
-    def _update_z_iface(self, z_nbdevice, z_iface, vlan, type):
-        """Create switch interface if needed and set vlan info."""
-        try:
-            # Try to find if the interface exists
-            z_nbiface = z_nbdevice.interfaces.get(name=z_iface)
-        except ObjectDoesNotExist:  # If interface doesn't exist: create it
-            z_nbiface = Interface(name=z_iface,
-                                  mgmt_only=False,
-                                  device=z_nbdevice,
-                                  type=type)
-            z_nbiface.save()
-            self.log_success(f"{z_nbdevice}: created interface {z_iface}.")
-
-        # Then configure it
-        z_nbiface.mode = 'access'
-        z_nbiface.untagged_vlan = vlan
-        z_nbiface.enabled = True
-        z_nbiface.mtu = 9192
-        z_nbiface.save()
-        self.log_success(f"{z_nbiface.device}:{z_nbiface} configured vlan.")
-        return z_nbiface
 
     def _assign_mgmt(self, device):
         """Create a management interface in the device and assign to it a management IP."""
