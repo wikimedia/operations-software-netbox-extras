83fb6b3189f7eb303661670c1a6668d1f59932a9
jbond <jbond@wikimedia.org>
2022-06-21 12:58:27 +0200

    getstats: Delete old versions of this report before running
    This CR updates the job so that it first deletes all previous versions
    of this report before running.  The code is based on the PR that added
    the JOBRESULT_RETENTION config item[1]
    
    [1]https://github.com/netbox-community/netbox/pull/8957
    
    Bug: T311048
    Change-Id: I8895a851592036fe34d1b24e42953510cbc754e6

--- customscripts/getstats.py
+++ customscripts/getstats.py
@@ -1,19 +1,59 @@
 """Produce Prometheus-formatted statistics about devices."""
+import sys
 
 from collections import Counter
+from datetime import timedelta
+from pathlib import Path
 from io import StringIO
+
+from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
 
 from dcim.models import Device
 
+from extras import models
+from extras.choices import JobResultStatusChoices
 from extras.scripts import Script
 
 
+def get_module(module):
+    """Get the module of this file handling the use case if its called as a script
+
+    Arguments
+        module: the value of __module__
+
+    Returns:
+        A string representing the module
+    """
+    if module != '__main__':
+        return module
+    return Path(sys.modules[module].__file__).stem
+
+
 class GetDeviceStats(Script):
+    """Get device statistics"""
+
     class Meta:
+        """Metadata"""
         name = "Get Device Statistics"
         description = "Dump a set of statistics about various devices for Prometheus."
 
     def run(self, data, commit):
+        """The run method"""
+        # Delete old versions of this report
+        obj_type = ContentType.objects.get_for_model(models.Script)
+        name = ".".join((get_module(self.__module__), self.__class__.__name__))
+        # Keep any reports from the last 5 minutes to make this less racy
+        cutoff = timezone.now() - timedelta(minutes=5)
+        jobs = models.JobResult.objects.filter(
+            obj_type=obj_type,
+            name=name,
+            status__in=JobResultStatusChoices.TERMINAL_STATE_CHOICES,
+            created__lt=cutoff,
+        )
+        # Make sure we call delete on each job to trigger any customized delete methods
+        for job in jobs:
+            job.delete()
         counts = Counter()
         output = StringIO()
         for device in Device.objects.all().values_list(
