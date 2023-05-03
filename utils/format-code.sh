7f571e9b6b54cd9ed378dc34d1ab757c94ce2d3b
Arzhel Younsi <ayounsi@wikimedia.org>
2023-05-03 09:11:39 +0200

    Add style checker and auto-formater to tox
    Change-Id: Iea5ee9585f7e662580e8466250d2b7f33decef97

---
+++ utils/format-code.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+# Copied from the Spicerack repo
+
+if [[ -n "$(git diff --name-only --diff-filter=d HEAD)" ]]; then
+    echo "Using staged and unstaged files"
+    REVISION="HEAD"
+else
+    echo "Using files changed in the latest commit"
+    REVISION="HEAD^"
+fi
+
+FILES=( $(git diff --name-only --diff-filter=d "${REVISION}" | grep '\.py$' || true) )
+
+if [[ "${#FILES[@]}" -eq "0" ]]; then
+    echo "No Python files to format"
+    exit 0
+fi
+
+black "${FILES[@]}"
+isort --apply "${FILES[@]}"
