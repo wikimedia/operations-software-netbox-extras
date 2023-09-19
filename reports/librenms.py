74b63d6a1a64f1fb8b9a2c75d58a0d8fd9e0b63e
Arzhel Younsi <ayounsi@wikimedia.org>
2023-09-19 09:15:49 +0200

    LibreNMS report: use black formating
    Change-Id: I1a2306f0b80480b3de57c8a749b257378dca943f

--- reports/librenms.py
+++ reports/librenms.py
@@ -13,11 +13,23 @@ from extras.reports import Report
 CONFIG_FILE = "/etc/netbox/reports.cfg"
 
 # Netbox system states to check.
-INCLUDE_STATUSES = (DeviceStatusChoices.STATUS_ACTIVE, DeviceStatusChoices.STATUS_STAGED)
+INCLUDE_STATUSES = (
+    DeviceStatusChoices.STATUS_ACTIVE,
+    DeviceStatusChoices.STATUS_STAGED,
+)
 
 # Netbox roles to check (slugs)
 # These are checked against the devices we get from LibreNMS
-INCLUDE_DEVICE_ROLES_LNMS_CHECK = ("asw", "msw", "cr", "mr", "pfw", "pdu", "scs", "cloudsw")
+INCLUDE_DEVICE_ROLES_LNMS_CHECK = (
+    "asw",
+    "msw",
+    "cr",
+    "mr",
+    "pfw",
+    "pdu",
+    "scs",
+    "cloudsw",
+)
 # These are used in every other check (the above minus scs which report incorrectly
 # in LibreNMS for serial numbers and device types.
 INCLUDE_DEVICE_ROLES = ("asw", "msw", "cr", "mr", "pfw", "pdu", "cloudsw")
@@ -47,10 +59,12 @@ INVENTORY_MANUFACTURERS = ("juniper",)
 MODEL_EQUIVS_INVENTORY = {"juniper ex4300-48t": "juniper routing engine"}
 
 # See T331519
-MODEL_EQUIVS_DEVICES = {"qfx5120-48y-afi": "jnp48y8c-chas",
-                        "qfx5120-48y-afi2": "jnp48y8c-chas",
-                        "qfx5120-32c-afi": "qfx5120-32c",
-                        "qfx5120-32c-afi2": "qfx5120-32c"}
+MODEL_EQUIVS_DEVICES = {
+    "qfx5120-48y-afi": "jnp48y8c-chas",
+    "qfx5120-48y-afi2": "jnp48y8c-chas",
+    "qfx5120-32c-afi": "qfx5120-32c",
+    "qfx5120-32c-afi2": "qfx5120-32c",
+}
 
 
 class LibreNMSData:
@@ -58,7 +72,9 @@ class LibreNMSData:
 
     def __init__(self, host, port, user, password, database):
         """Populate internal state from the LibreNMS database given MySQL connection parameters."""
-        connection = pymysql.connect(host=host, port=int(port), user=user, password=password, database=database)
+        connection = pymysql.connect(
+            host=host, port=int(port), user=user, password=password, database=database
+        )
 
         self.device_duplicates = {}
         self.inventory_duplicates = {}
@@ -127,7 +143,13 @@ class LibreNMS(Report):
         configfile = configparser.ConfigParser()
         configfile.read(CONFIG_FILE)
         config = configfile["librenms"]
-        return LibreNMSData(config["dbhost"], config["dbport"], config["user"], config["password"], config["database"])
+        return LibreNMSData(
+            config["dbhost"],
+            config["dbport"],
+            config["user"],
+            config["password"],
+            config["database"],
+        )
 
     def test_nb_net_in_librenms(self):
         """Check that every Device in the asw, pfw, msw, and cr classes in Netbox are `devices` in LibreNMS,
@@ -159,17 +181,24 @@ class LibreNMS(Report):
                 if not dev.primary_ip:
                     no_ip += 1
                 else:
-                    self.log_failure(dev, f"missing Netbox device from LibreNMS of role {dev.device_role.slug}")
+                    self.log_failure(
+                        dev,
+                        f"missing Netbox device from LibreNMS of role {dev.device_role.slug}",
+                    )
 
         if no_ip:
-            self.log_info(None, f"{no_ip} Netbox devices with no IP and not in LibreNMS")
+            self.log_info(
+                None, f"{no_ip} Netbox devices with no IP and not in LibreNMS"
+            )
         self.log_success(None, f"{success} Netbox devices in LibreNMS")
 
     def test_nb_inventory_in_librenms(self):
         """Check that every InventoryItem attached to a Device in Netbox, is in `entPhysical` table in librenms, matched
         by serial number."""
         success = 0
-        parents = self._device_query.values_list("pk", flat=True).filter(device_role__slug__in=INCLUDE_DEVICE_ROLES)
+        parents = self._device_query.values_list("pk", flat=True).filter(
+            device_role__slug__in=INCLUDE_DEVICE_ROLES
+        )
         for inventory_item in (
             InventoryItem.objects.filter(device_id__in=parents)
             .exclude(serial__isnull=True)
@@ -181,7 +210,9 @@ class LibreNMS(Report):
                 inventory_item.serial not in self.librenms.inventory
                 and inventory_item.device.site.slug not in EXCLUDE_SITES
             ):
-                self.log_failure(inventory_item, "missing Netbox inventory item from LibreNMS")
+                self.log_failure(
+                    inventory_item, "missing Netbox inventory item from LibreNMS"
+                )
             else:
                 success += 1
 
@@ -190,9 +221,9 @@ class LibreNMS(Report):
     def test_librenms_in_nb(self):
         """Check that every `device` in LibreNMS exists as a Device in Netbox, matched by serial number."""
         success = 0
-        devserials = self._device_query.filter(device_role__slug__in=INCLUDE_DEVICE_ROLES_LNMS_CHECK).values_list(
-            "serial", flat=True
-        )
+        devserials = self._device_query.filter(
+            device_role__slug__in=INCLUDE_DEVICE_ROLES_LNMS_CHECK
+        ).values_list("serial", flat=True)
         for serial, device in self.librenms.devices.items():
             if serial not in devserials:
                 self.log_failure(
@@ -217,18 +248,25 @@ class LibreNMS(Report):
         as general a check as possible without special exceptions).
         """
         success = 0
-        for device in self._device_query.filter(device_role__slug__in=INCLUDE_DEVICE_ROLES).exclude(MODEL_EXCLUDES):
+        for device in self._device_query.filter(
+            device_role__slug__in=INCLUDE_DEVICE_ROLES
+        ).exclude(MODEL_EXCLUDES):
             nb_vendor_string = str(device.device_type.manufacturer).lower()
             nb_model_string = str(device.device_type.model).lower()
             nb_vendor_model_string = " ".join((nb_vendor_string, nb_model_string))
             # Either the hardware or description has both the vendor and the model, discretely.
             if device.serial in self.librenms.devices:
                 lnms_device_hardware = self.librenms.devices[device.serial]["hardware"]
-                lnms_device_description = self.librenms.devices[device.serial]["description"]
-                lnms_device_harddesc = lnms_device_hardware + " " + lnms_device_description
+                lnms_device_description = self.librenms.devices[device.serial][
+                    "description"
+                ]
+                lnms_device_harddesc = (
+                    lnms_device_hardware + " " + lnms_device_description
+                )
                 if nb_vendor_string in lnms_device_harddesc and (
                     nb_model_string in lnms_device_harddesc
-                    or MODEL_EQUIVS_DEVICES.get(nb_model_string, "BADMATCH") in lnms_device_harddesc
+                    or MODEL_EQUIVS_DEVICES.get(nb_model_string, "BADMATCH")
+                    in lnms_device_harddesc
                 ):
                     success += 1
                 elif device.site.slug not in EXCLUDE_SITES:
@@ -255,7 +293,8 @@ class LibreNMS(Report):
                         nb_vendor_string in librenms_vendor_model_string
                         and nb_model_string in librenms_vendor_model_string
                     )
-                    or MODEL_EQUIVS_INVENTORY.get(nb_vendor_model_string, "BADMATCH") in librenms_vendor_model_string
+                    or MODEL_EQUIVS_INVENTORY.get(nb_vendor_model_string, "BADMATCH")
+                    in librenms_vendor_model_string
                 ):
                     success += 1
                 elif device.site.slug not in EXCLUDE_SITES:
@@ -267,4 +306,6 @@ class LibreNMS(Report):
                         ).format(nb_vendor_model_string, librenms_vendor_model_string),
                     )
 
-        self.log_success(None, f"{success} LibreNMS hardware and manufacturer matches in Netbox")
+        self.log_success(
+            None, f"{success} LibreNMS hardware and manufacturer matches in Netbox"
+        )
